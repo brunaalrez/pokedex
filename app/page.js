86a5f13f72c26240d5b1a69c28707f30176a7699
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-02-13 20:59:30 -0300

    segundo

--- app/page.js
+++ app/page.js
@@ -1,7 +1,30 @@
-import Image from "next/image";
+'use client'
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [pokemon, alteraPokemon] = useState([]);
+  const [pesquisa, alteraPesquisa] = useState("");
+
+  async function busca(){
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
+    const data = response.data;
+    console.log(data);
+  }
   return (
-    
+    <div>
+      <h1>Pokedex</h1>
+
+      <p>Digite o nome de u Pokémon:</p>
+      <input/>
+      <br/>
+      <button onClick={()=> busca()}>Pesquisar</button>
+
+      <hr/>
+
+      <h2>Nome</h2>
+      <p><strong>ID: </strong>??</p>
+      <img src=""/>
+    </div>
   );
 }
