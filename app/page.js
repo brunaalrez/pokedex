96f84b4f199c6b07fd015d1aa0729fcaee43dfa9
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-02-14 21:56:17 -0300

    finalizado

--- app/page.js
+++ app/page.js
@@ -1,30 +1,62 @@
 'use client'
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [pokemon, alteraPokemon] = useState([]);
   const [pesquisa, alteraPesquisa] = useState("");
 
+  const [erroPesquisa, alteraErro] = useState(false);
+
   async function busca(){
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
-    const data = response.data;
-    console.log(data);
+    try{
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" +pesquisa);
+
+      const data = response.data;
+      alteraPokemon([data]);
+      alteraErro(false);
+    }catch(e){
+      alteraErro(true);
+    }
   }
+
+  async function buscaTodos(){
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+    const data = response.data.results;
+    alteraPokemon(data);
+  }
+
+  useEffect(()=>{
+    buscaTodos();
+  },[])
   return (
     <div>
       <h1>Pokedex</h1>
 
       <p>Digite o nome de u Pokémon:</p>
-      <input/>
+      <input onChange={(e)=> alteraPesquisa(e.target.value)}/>
       <br/>
       <button onClick={()=> busca()}>Pesquisar</button>
 
-      <hr/>
+      {erroPesquisa == true && <p className="text-red-500">Erro ao pesquisa</p>}
 
-      <h2>Nome</h2>
-      <p><strong>ID: </strong>??</p>
-      <img src=""/>
+      <hr/>
+      {
+        pokemon == 0 ?
+          <p>Carregando...</p>
+        :
+          pokemon.map((i, index) =>
+            <div>
+              <h2>{i.name}</h2>
+              <p><strong>ID: </strong>{i.id ? i.id : index+1}</p>
+              <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"+(i.id ? i.id : index + 1)+".gif"}/>
+            </div>
+          )
+         
+         
+      }
+
+      
     </div>
   );
 }
